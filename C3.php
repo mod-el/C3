aa77eab53aca026d5cd92c92d626516008aa99be
Domenico Giambra <zorinik@gmail.com>
2018-06-30 19:44:09 +0200

    Basic support for pie charts

--- C3.php
+++ C3.php
@@ -4,6 +4,11 @@ use Model\Core\Module;
 
 class C3 extends Module
 {
+	/**
+	 * @param $list
+	 * @param array $options
+	 * @throws \Exception
+	 */
 	public function lineChart($list, array $options = [])
 	{
 		$options = array_merge([
@@ -42,7 +47,9 @@ class C3 extends Module
 				'bindto' => '#' . $options['id'],
 				'data' => [
 					'x' => 'x',
-					'columns' => $chartColumns,
+					'columns' => [
+						['x'],
+					],
 				],
 			];
 
@@ -78,7 +85,125 @@ class C3 extends Module
 			break;
 			}
 			?>
-			c3.generate(chartOptions);
+			if (typeof c3_chartes === 'undefined')
+				var c3_chartes = {};
+			c3_chartes['<?= entities($options['id']) ?>'] = c3.generate(chartOptions);
+			setTimeout(function () {
+				c3_chartes['<?= entities($options['id']) ?>'].load({
+					'x': 'x',
+					'columns': <?=json_encode($chartColumns)?>
+				});
+			}, 100);
+		</script>
+		<?php
+	}
+
+	/**
+	 * @param $list
+	 * @param array $options
+	 * @throws \Exception
+	 */
+	public function pieChart($list, array $options = [])
+	{
+		$options = array_merge([
+			'id' => 'pie-chart',
+			'field' => null,
+			'label' => null,
+			'label-type' => null,
+			'values-type' => null, // supported at the moment: price
+		], $options);
+
+		if (!$options['label'] or !$options['field'])
+			return;
+
+		$chartColumns = [];
+
+		$numbersDirection = null;
+		foreach ($list as $elIdx => $el) {
+			if (is_object($el)) {
+				$form = $el->getForm();
+				if ($form[$options['label']])
+					$label = $form[$options['label']]->getText();
+				else
+					$label = $el[$options['label']];
+			} else {
+				$label = $el[$options['label']] ?? '';
+			}
+
+			$value = $el[$options['field']];
+			if (!is_numeric($value)) {
+				echo 'Unsupported non-numeric value for pie chart';
+				return;
+			}
+
+			if ($numbersDirection) {
+				if (($numbersDirection == 1 and $value < 0) or ($numbersDirection == -1 and $value > 0)) {
+					echo 'Cannot mix negative and positive numbers in a pie chart';
+					return;
+				}
+			} else {
+				$numbersDirection = $value > 0 ? 1 : -1;
+			}
+			if ($value < 0)
+				$value = abs($value);
+
+			$chartColumns[] = [
+				$label,
+				$value,
+			];
+		}
+		?>
+		<div id="<?= entities($options['id']) ?>"></div>
+		<script>
+			<?php
+			$chartOptions = [
+				'bindto' => '#' . $options['id'],
+				'data' => [
+					'type' => 'pie',
+					'columns' => [],
+				],
+			];
+
+			switch ($options['label-type']) {
+				case 'datetime':
+					if (!isset($options['label-format']))
+						$options['label-format'] = '%d/%m/%Y';
+
+					$chartOptions['axis'] = [
+						'x' => [
+							'type' => 'timeseries',
+							'tick' => [
+								'format' => $options['label-format'],
+							],
+						],
+					];
+					break;
+			}
+			?>
+			var chartOptions = <?=json_encode($chartOptions, JSON_PRETTY_PRINT)?>;
+			<?php
+			switch ($options['values-type']) {
+			case 'price':
+			?>
+			chartOptions['tooltip'] = {
+				'format': {
+					'value': value => {
+						return makePrice(value);
+					},
+				}
+			};
+			<?php
+			break;
+			}
+			?>
+			if (typeof c3_chartes === 'undefined')
+				var c3_chartes = {};
+			c3_chartes['<?= entities($options['id']) ?>'] = c3.generate(chartOptions);
+			setTimeout(function () {
+				c3_chartes['<?= entities($options['id']) ?>'].load({
+					'columns': <?=json_encode($chartColumns)?>
+				});
+			}, 100);
 		</script>
 		<?php
 	}
